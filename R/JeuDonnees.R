c2eabb2e3818f2ccdbe88ecd69725626c1d16f23
Francis Duval <francisduval37@gmail.com>
2023-02-28 16:05:48 -0500

    Modifs classe R6 JeuDonnees

--- R/JeuDonnees.R
+++ R/JeuDonnees.R
@@ -8,13 +8,20 @@ JeuDonnees <-
         tele_ml_data = NULL,
         nn_data = NULL,
         
+        # ----------
+        
         initialize = function(data) {
+          
+          # -----
+          
           self$classic_ml_data <- 
             data %>% 
             group_by(vin) %>% 
             slice(1) %>% 
             select(vin, expo:years_licensed, claim_ind_cov_1_2_3_4_5_6) %>% 
             ungroup()
+          
+          # -----
           
           self$tele_ml_data <- 
             data %>% 
@@ -56,33 +63,139 @@ JeuDonnees <-
             ) %>% 
             ungroup()
           
-          compute_proportion <- function(time_start_vec, time_end_vec, nb_sec = 3600) {
-            vv <- vector(mode = "numeric", length = 86400)
-            n <- length(time_start_vec)
-            
-            for(i in 1:n) {
-              ind <- seq(time_start_vec[i], time_end_vec[i])
-              vv[ind] <- vv[ind] + 1
+          # -----
+          
+          compute_d_col <- function(data) {
+            data %>%
+              group_by(vin) %>%
+              summarise(distance = sum(distance)) 
+          }
+          
+          # -----
+          
+          compute_h_cols <- function(data) {
+            compute_hour_vec <- function(time_start_vec, time_end_vec) {
+              seconds_vec <- vector(mode = "numeric", length = 86400)
+              
+              for(i in seq_along(time_start_vec)) {
+                ind <- seq(as.numeric(time_start_vec[i]), as.numeric(time_end_vec[i]))
+                seconds_vec[ind] <- seconds_vec[ind] + 1
+              }
+              
+              hours_vec <- map_dbl(1:24, ~ sum(seconds_vec[(3600 * (.x - 1) + 1):(3600 * .x)]))
+              
+              return(hours_vec / sum(hours_vec))
             }
             
-            tot_sec <- map_dbl(1:(86400 / nb_sec), ~ sum(vv[(nb_sec * (. - 1) + 1):(nb_sec * .)]))
-            res <- tot_sec / sum(tot_sec)
+            h_cols <- 
+              data %>%
+              group_by(vin) %>%
+              summarise(time_cols = list(compute_hour_vec(time_start, time_end))) %>%
+              unnest(cols = time_cols) %>%
+              mutate(names = rep(glue("h_{1:24}"), nrow(.) / 24)) %>%
+              pivot_wider(names_from = names, values_from = time_cols)
+            
+            return(h_cols)
+          }
+          
+          # -----
+          
+          compute_p_cols <- function(data) {
+            compute_days_vec <- function(datetime_start_vec, datetime_end_vec) {
+              weekday_vec_start <- wday(datetime_start_vec, week_start = 1)
+              weekday_vec_end <- wday(datetime_end_vec, week_start = 1)
+              
+              secs_since_midnight_vec_start <- as.numeric(as_hms(datetime_start_vec))
+              secs_since_midnight_vec_end <- as.numeric(as_hms(datetime_end_vec))
+              
+              sec_since_monday_vec_start <- (weekday_vec_start - 1) * 86400 + secs_since_midnight_vec_start
+              sec_since_monday_vec_end <- (weekday_vec_end - 1) * 86400 + secs_since_midnight_vec_end
+              
+              seconds_vec <- vector(mode = "numeric", length = 604800)
+              
+              for(i in seq_along(datetime_start_vec)) {
+                ind <- seq(sec_since_monday_vec_start[i], sec_since_monday_vec_end[i])
+                seconds_vec[ind] <- seconds_vec[ind] + 1
+              }
+              
+              days_vec <- map_dbl(1:7, ~ sum(seconds_vec[(86400 * (.x - 1) + 1):(86400 * .x)]))
+              
+              return(days_vec / sum(days_vec))
+            }
+            
+            res <- 
+              data %>%
+              group_by(vin) %>%
+              summarise(days_cols = list(compute_days_vec(datetime_start, datetime_end))) %>%
+              unnest(cols = days_cols) %>%
+              mutate(names = rep(glue("p_{1:7}"), nrow(.) / 7)) %>%
+              pivot_wider(names_from = names, values_from = days_cols)
             
             return(res)
           }
           
-          self$nn_data <-
-            data %>%
-            group_by(vin) %>%
-            summarise(
-              prop = list(compute_proportion(time_start, time_end)),
-              distance = sum(distance),
-              claim_ind_cov_1_2_3_4_5_6 = first(claim_ind_cov_1_2_3_4_5_6)
-            ) %>%
-            unnest(cols = c(prop)) %>%
-            mutate(names = rep(glue("t_{1:24}"), nrow(.) / 24)) %>%
-            select(vin, prop, names, claim_ind_cov_1_2_3_4_5_6, distance) %>%
-            pivot_wider(names_from = names, values_from = prop)
+          # -----
+          
+          compute_speed_buckets <- function(speed_vec, duration_vec, nb_buckets) {
+            bucket_vec <- vector(mode = "numeric", length = nb_buckets)
+            
+            for(i in seq(1, nb_buckets)) {
+              bucket_vec[i] <- sum(duration_vec[speed_vec > 10 * (i - 1) & speed_vec <= 10 * i])
+            }
+            
+            bucket_vec[nb_buckets] <- bucket_vec[nb_buckets] + sum(duration_vec[speed_vec > 10 * i])
+            
+            return(bucket_vec / sum(bucket_vec))
+          }
+          
+          # -----
+          
+          compute_vmo_cols <- function(data, nb_buckets = 14) {
+            res <- 
+              data %>%
+              group_by(vin) %>%
+              summarise(vmo_cols = list(compute_speed_buckets(avg_speed, duration, nb_buckets))) %>%
+              unnest(cols = vmo_cols) %>%
+              mutate(names = rep(glue("vmo_{1:nb_buckets}"), nrow(.) / nb_buckets)) %>%
+              pivot_wider(names_from = names, values_from = vmo_cols)
+            
+            return(res)
+          }
+          
+          # -----
+          
+          compute_vma_cols <- function(data, nb_buckets = 16) {
+            res <- 
+              data %>%
+              group_by(vin) %>%
+              summarise(vma_cols = list(compute_speed_buckets(max_speed, duration, nb_buckets))) %>%
+              unnest(cols = vma_cols) %>%
+              mutate(names = rep(glue("vmo_{1:nb_buckets}"), nrow(.) / nb_buckets)) %>%
+              pivot_wider(names_from = names, values_from = vma_cols)
+            
+            return(res)
+          }
+          
+          # -----
+          
+          compute_nn_data <- function(data) {
+            d_dat <- compute_d_col(data)
+            h_dat <- compute_h_cols(data)
+            p_dat <- compute_p_cols(data)
+            vmo_dat <- compute_vmo_cols(data)
+            vma_dat <- compute_vma_cols(data)
+          }
+          
+          # -----
+          
+          self$nn_data <- compute_nn_data(data)
+          
+          
+          # -----
         }
+        
+        # ----------
+        
       )
   )
+
